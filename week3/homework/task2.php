452bd8e73909af946770880adf5445a26131b412
mariami01 <zenaishvilimariam@gmail.com>
2021-03-31 16:00:46 +0400

    done

--- week3/homework/task2.php
+++ week3/homework/task2.php
@@ -6,7 +6,18 @@
 <body>
     
     <?php
-        
+
+        // $matrix = [];
+
+        // echo "<table>"
+        // foreach($matrix as $i){
+        //     echo "<tr>";
+        //     foreach($matrix as $j){
+        //         echo "<td>" . rand(10, 100). "</td>";
+        //     }
+        //     echo "</tr>";
+        // }
+        // echo "</table>";
     ?>
 </body>
 </html>
